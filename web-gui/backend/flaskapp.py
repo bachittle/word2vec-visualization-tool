f8057e509e803506a1fe834b7136656eebbd5ac9
Bailey Chittle <bachittle@gmail.com>
2021-08-20 21:26:48 -0400

    init flask app

--- web-gui/backend/flaskapp.py
+++ web-gui/backend/flaskapp.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/word2vec')
+def word2vec_handler():
+	text_file = request.form['text_file']
+	text_input = request.form['text_input']
+	
+	# If both are given, text_file goes first. 
+	if text_file:
+		print(text_file)
+	elif text_input:
+		print(text_input)
