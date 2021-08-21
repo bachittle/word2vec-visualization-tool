c66ac4a99dd1ab6ec8066d9079ba74dc114a91f7
Bailey Chittle <bachittle@gmail.com>
2021-08-21 02:36:08 +0000

    removed index page, only w2v route here

--- web-gui/backend/flaskapp.py
+++ web-gui/backend/flaskapp.py
@@ -2,10 +2,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('index.html')
-
 @app.route('/word2vec')
 def word2vec_handler():
 	text_file = request.form['text_file']
